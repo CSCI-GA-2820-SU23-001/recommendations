8116bbe51deae12a112717e8ffd00fac0894bdc2
harsha031098 <harsha.joshi@nyu.com>
2023-06-27 07:12:26 +0000

    adding attributes to model

--- service/models.py
+++ service/models.py
@@ -1,44 +1,62 @@
 """
-Models for YourResourceModel
+Models for Recommendation
 
 All of the models are stored in this module
 """
 import logging
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
+from datetime import date
+from flask import Flask
 
 logger = logging.getLogger("flask.app")
 
 # Create the SQLAlchemy object to be initialized later in init_db()
 db = SQLAlchemy()
 
+class RecommendationType(Enum):
+    """Enumeration of valid Recommendation Types"""
+    UPSELL = 0
+    CROSS_SELL = 1
+    FREQ_BOUGHT_TOGETHER = 2
+    RECOMMENDED_FOR_YOU =3
+    TRENDING = 4
+    UNKOWN = 5
 
 # Function to initialize the database
 def init_db(app):
     """ Initializes the SQLAlchemy app """
-    YourResourceModel.init_db(app)
+    Recommendation.init_db(app)
 
 
 class DataValidationError(Exception):
     """ Used for an data validation errors when deserializing """
 
 
-class YourResourceModel(db.Model):
+class Recommendation(db.Model):
     """
-    Class that represents a YourResourceModel
+    Class that represents a Recommendation
     """
 
     app = None
 
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63))
+    name = db.Column(db.String(63), nullable=False)
+    product_id = db.Column(db.Integer, nullable=False)
+    create_date= db.Column(db.Date(), nullable=False, default=date.today())
+    update_date= db.Column(db.Date(), nullable=False, default=date.today())
+    
+    recommendation_type = db.Column(
+        db.Enum(RecommendationType), nullable=False, server_default=(RecommendationType.UNKOWN.name)
+    )
 
     def __repr__(self):
-        return f"<YourResourceModel {self.name} id=[{self.id}]>"
+        return f"<Recommendation {self.name} id=[{self.id}]>"
 
     def create(self):
         """
-        Creates a YourResourceModel to the database
+        Creates a Recommendation to the database
         """
         logger.info("Creating %s", self.name)
         self.id = None  # pylint: disable=invalid-name
@@ -47,37 +65,48 @@ class YourResourceModel(db.Model):
 
     def update(self):
         """
-        Updates a YourResourceModel to the database
+        Updates a Recommendation to the database
         """
         logger.info("Saving %s", self.name)
         db.session.commit()
 
     def delete(self):
-        """ Removes a YourResourceModel from the data store """
+        """ Removes a Recommendation from the data store """
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
     def serialize(self):
-        """ Serializes a YourResourceModel into a dictionary """
-        return {"id": self.id, "name": self.name}
+        """ Serializes a Recommendation into a dictionary """
+        return {
+            "id": self.id, 
+            "name": self.name,
+            "product_id": self.product_id,
+            "recommendation_type": self.recommendation_type,
+            "create_date": self.create_date,
+            "update_date": self.update_date
+            }
 
     def deserialize(self, data):
         """
-        Deserializes a YourResourceModel from a dictionary
+        Deserializes a Recommendation from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
         """
         try:
             self.name = data["name"]
+            self.product_id = data["product_id"]
+            self.recommendation_type = data["recommendation_type"]
+            self.create_date = data["create_date"]
+            self.update_date = data["update_date"]
         except KeyError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: missing " + error.args[0]
+                "Invalid Recommendation: missing " + error.args[0]
             ) from error
         except TypeError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: body of request contained bad or no data - "
+                "Invalid Recommendation: body of request contained bad or no data - "
                 "Error message: " + error
             ) from error
         return self
@@ -94,22 +123,22 @@ class YourResourceModel(db.Model):
 
     @classmethod
     def all(cls):
-        """ Returns all of the YourResourceModels in the database """
-        logger.info("Processing all YourResourceModels")
+        """ Returns all of the Recommendations in the database """
+        logger.info("Processing all Recommendations")
         return cls.query.all()
 
     @classmethod
     def find(cls, by_id):
-        """ Finds a YourResourceModel by it's ID """
+        """ Finds a Recommendation by it's ID """
         logger.info("Processing lookup for id %s ...", by_id)
         return cls.query.get(by_id)
 
     @classmethod
     def find_by_name(cls, name):
-        """Returns all YourResourceModels with the given name
+        """Returns all Recommendations with the given name
 
         Args:
-            name (string): the name of the YourResourceModels you want to match
+            name (string): the name of the Recommendations you want to match
         """
         logger.info("Processing name query for %s ...", name)
         return cls.query.filter(cls.name == name)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,32 +1,44 @@
 """
-Test cases for YourResourceModel Model
+Test cases for Recommendation Model
 
 """
 import os
 import logging
 import unittest
-from service.models import YourResourceModel, DataValidationError, db
-
+from service.models import Recommendation, DataValidationError, db
+from service import app
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/testdb"
+)
 
 ######################################################################
-#  YourResourceModel   M O D E L   T E S T   C A S E S
+#  Recommendation   M O D E L   T E S T   C A S E S
 ######################################################################
-class TestYourResourceModel(unittest.TestCase):
-    """ Test Cases for YourResourceModel Model """
+class TestRecommendation(unittest.TestCase):
+    """ Test Cases for Recommendation Model """
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        Recommendation.init_db(app)
 
     @classmethod
     def tearDownClass(cls):
-        """ This runs once after the entire test suite """
+        pass
 
     def setUp(self):
-        """ This runs before each test """
+        """This runs before each test"""
+        db.drop_all()
+        db.create_all()
 
     def tearDown(self):
-        """ This runs after each test """
+        """This runs after each test"""
+        db.session.remove()
+        db.drop_all()
 
     ######################################################################
     #  T E S T   C A S E S
