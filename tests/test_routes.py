044e1b7a9b52b10b591fc8249c7eff3a916cab93
Jiayi <jyl.personal668@gmail.com>
2023-07-03 06:48:47 +0000

    delete the redundant testcase for deletion

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -207,18 +207,6 @@ class TestYourResourceServer(TestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
-    # TEST CASES FOR DELETE #
-
-    # def test_delete_recommendation(self):
-    #     """It should Delete a Recommendation"""
-    #     test_recommendation = self._create_recommendations(1)[0]
-    #     response = self.client.delete(f"{BASE_URL}/{test_recommendation.id}")
-    #     self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-    #     self.assertEqual(len(response.data), 0)
-    #     # makes sure they are deleted
-    #     response = self.client.get(f"{BASE_URL}/{test_recommendation.id}")
-    #     self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
 
     # TEST CASES FOR DELETE #
 
